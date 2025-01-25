67a1e820e34d71871f53d3d5b1ad9482094acf6f
Md.Naimur Rahman <74141098+Akashbk201@users.noreply.github.com>
2025-01-26 03:30:55 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -29,7 +29,7 @@ function showPopup(meal) {
   popUp.classList.add("visible");
   popUp.classList.remove("invisible");
   popUp.innerHTML = `
-    <div class="w-4/5 md:w-3/5 h-48 md:h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center overflow-y-scroll md:overflow-y-auto">
+    <div class="w-4/5 md:w-3/5 h-[70vh] md:h-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center overflow-y-scroll md:overflow-y-auto">
             <div class=" bg-orange-100 justify-center items-center text-left py-12 px-8 rounded-md">
               <div class="text-right"><button onclick="closePopup()"
                     class="inline-block text-base font-normal bg-red-600 text-white px-2 py-2 rounded-md cursor-pointer absolute top-3 right-3"><svg class="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path></svg></button></div>
